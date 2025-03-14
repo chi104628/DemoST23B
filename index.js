f0229e64de3db7e938f649d140c8bd681e01207b
chi104628 <chi104628@donga.edu.vn>
2025-03-15 01:44:36 +0700

    first commit

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ const express = require('express');
 const path = require('path'); 
 const app = express();
 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 5000; 
 
 app.set("views", path.join(__dirname, "views")); 
 app.set("view engine", "ejs");
 app.use(express.static('public'))
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route trang chủ
 app.get('/', (req, res) => {
-    res.render('index'); 
+    // Lấy ngày hiện tại
+    const days = ['Chủ Nhật', 'Thứ hai', 'Thứ ba', 'Thứ tư', 'Thứ năm', 'Thứ sáu', 'Thứ bảy'];
+    const today = new Date();
+    const dayOfWeek = days[today.getDay()];
+
+    res.render('home', { today: dayOfWeek });
 });
+app.get('/shop', (req, res) => {
+    const products = [
+        { name: "Sách Lập Trình", price: 150000, image: "/images/book.jpg" },
+        { name: "Bút Máy", price: 50000, image: "/images/book.jpg" },
+        { name: "Laptop", price: 15000000, image: "/images/book.jpg" }
+    ];
+});
+
+    app.get('/shop', (req, res) => {
+        res.render('shop', { products });
+ });
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
